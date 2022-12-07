644206c7a7259eb591d02c00ae3be16e792b5b9d
Fededada23 <dadamofederico@gmail.com>
2022-12-07 10:04:21 +0100

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function generateGrid()
+{
+    document.querySelector('.grid').innerHTML=''
+    for(let i = 0; i<100; i++){
+        const cell=document.createElement('div');
+        cell.classList.add('square')
+        cell.innerText = i + 1
+        cell.addEventListener('click', function(){
+            this.classList.toggle('clicked')
+            console.log(this.innerHTML);
+        })
+
+        document.querySelector('.grid').appendChild(cell)
+    }
+}
+
+
+
+document.getElementById('play-game').addEventListener('click', function(){
+    generateGrid();
+})
